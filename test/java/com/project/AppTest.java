0f3b128126bb03306913f7d9d068bef8a39a8ca9
PraveenS-786 <101189480+PraveenS-786@users.noreply.github.com>
2025-09-14 12:04:47 +0530

    maven file added

---
+++ test/java/com/project/AppTest.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
